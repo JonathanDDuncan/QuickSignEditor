168fe437ba835dece331e415e2b1f6a81b440001
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-08-19 07:36:46 -0600

    Edititing formating so that it shoes on top with SignMaker

--- build.cmd
+++ build.cmd
@@ -2,5 +2,5 @@ npm run build
 
 del C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\lib\elm.js
 move .\dist\*.js  C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\lib\elm.js
-
+copy .\src\styles\*.css  C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\css\
 pause
