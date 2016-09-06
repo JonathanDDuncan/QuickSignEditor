862e7aea713d315d42b5138cd05fd47db5c5bb34
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-09-05 20:43:47 -0600

    Fixing up keyboard

--- src/Keyboard/View.elm
+++ src/Keyboard/View.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Keyboard.Types exposing (..)
 import Array exposing (..)
+import Dict exposing (..)
 
 
 --import SubFeature.View exposing (root)
@@ -12,27 +13,48 @@ import Array exposing (..)
 root : Model -> Html Msg
 root model =
     div [ class "keyboard" ]
-        [ row model [1..14]
-        , row model [15..28]
-        , row model [29..41]
-        , row model [42..55]
-        , row model [56..63]
+        [ div [ class "alphabetic", style [ ( "width", "66%" ) ] ]
+            [ row model [1..14]
+            , row model [15..28]
+            , row model [29..41]
+            , row model [42..53]
+            , row model [54..60]
+            ]
+        , div
+            [ class "arrows", style [ ( "width", "14%" ) ] ]
+            [ div [ style [ ( "height", "40%" ), ( "margin-bottom", "17%" ) ] ]
+                [ row model [61..63]
+                , row model [64..66]
+                ]
+            , div [ style [ ( "height", "40%" ) ] ]
+                [ row model [67..67]
+                , row model [68..70]
+                ]
+            ]
+        , div
+            [ class "numeric", style [ ( "width", "20%" ) ] ]
+            [ row model [71..73]
+            , row model [74..77]
+            , row model [78..81]
+            , row model [82..85]
+            , row model [86..87]
+            ]
         ]
 
 
 row : Model -> List Int -> Html Msg
 row model nums =
-    div []
+    div [ class "row" ]
         (createkeys model nums)
 
 
 nkey : Model -> Int -> Html Msg
 nkey model n =
-    div []
+    div [ class "key" ]
         [ span []
-            [ text <| toString (getKeyCode n model) ]
+            []
         , span []
-            [ text "X" ]
+            [ text <| getKeyString n model ]
         ]
 
 
@@ -43,6 +65,18 @@ getKeyCode n model =
             nth n model.keyboardlayout.codes
     in
         Maybe.withDefault 0 keycode
+
+
+getKeyString : Int -> Model -> String
+getKeyString n model =
+    let
+        keycode =
+            Maybe.withDefault 0 (nth n model.keyboardlayout.codes)
+
+        keystring =
+            Dict.get keycode model.keycodedictionary
+    in
+        Maybe.withDefault (toString keycode) keystring
 
 
 nth : Int -> List a -> Maybe a
