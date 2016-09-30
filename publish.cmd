c7be59e73a46689b85082d061cf0750eed575432
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-09-30 13:24:35 -0600

    Publish changes test

--- publish.cmd
+++ publish.cmd
@@ -12,7 +12,7 @@ copy .\src\styles\*.css  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio
 
 copy .\src\sw10.min.js  published\ /y
 xcopy .\src\signmaker\*.*  published\signmaker /s /y
-
+rem Please commit any changes to the publish folder
 pause
 git branch -dr origin/gh-pages
 git subtree push --prefix published origin gh-pages
