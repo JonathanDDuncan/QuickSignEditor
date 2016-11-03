2aac700ad79bf9368e997e1c7b5316b60975628d
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-11-02 21:24:05 -0600

    Fixing copy over so that it will work properly in SignWriter Studio

--- copy over.cmd
+++ copy over.cmd
@@ -3,10 +3,14 @@ del src\signmaker\lib\elm.js
 move .\dist\*.js  src\signmaker\lib\elm.js
 copy .\src\styles\*.css  src\signmaker\css\
 copy .\src\images\*.*  src\signmaker\images\
-copy .\src\chooserclassification.json  src\signmaker\chooserclassification.json
+copy .\src\chooserclassification.js  src\signmaker\chooserclassification.js
 
-copy src\signmaker\lib\elm.js "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\lib\"
+
+copy .\src\signmaker\lib\elm.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\"
+copy .\src\chooserclassification.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\"
+	                    
 copy .\src\styles\*.css  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\css\"
 copy .\src\images\*.*  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\images\"
-copy .\src\chooserclassification.json  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\chooserclassification.json"
+copy .\src\chooserclassification.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\chooserclassification.js"
+
 pause
