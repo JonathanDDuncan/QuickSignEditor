f1a27988b0fa56ef8becef51a0208dbe7a68304c
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-11-14 19:11:33 -0600

    updating copy over command

--- copy over.cmd
+++ copy over.cmd
@@ -3,14 +3,16 @@ del src\signmaker\lib\elm.js
 move .\dist\*.js  src\signmaker\lib\elm.js
 copy .\src\styles\*.css  src\signmaker\css\
 copy .\src\images\*.*  src\signmaker\images\
-copy .\src\chooserclassification.js  src\signmaker\chooserclassification.js
-
+copy .\src\chooserclassification.js  src\signmaker\chooserclassification.js   
+copy .\src\symbolsize.js  src\signmaker\symbolsize.js  
 
 copy .\src\signmaker\lib\elm.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\"
 copy .\src\chooserclassification.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\"
-	                    
+copy .\src\symbolsize.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\"	                    
 copy .\src\styles\*.css  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\css\"
 copy .\src\images\*.*  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\images\"
-copy .\src\chooserclassification.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\chooserclassification.js"
-
+copy .\src\chooserclassification.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist
+\chooserclassification.js"
+copy .\src\symbolsize.js  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist
+\symbolsize.js"
 pause
