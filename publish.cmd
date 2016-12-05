002a33d5d882509fbd57fec8449df8c439f3678f
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-12-05 12:13:33 -0600

    Adding chooserclassification.js and symbolsize.js to published

--- publish.cmd
+++ publish.cmd
@@ -6,11 +6,16 @@ del src\signmaker\lib\elm.js
 
 move .\dist\*.js  src\signmaker\lib\elm.js
 copy .\src\styles\*.css  src\signmaker\css\
+copy .\src\styles\*.css  published\styles\
 
 copy src\signmaker\lib\elm.js "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\lib\"
 copy .\src\styles\*.css  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\css\"
 
+copy .\src\sw10.js  published\ /y
 copy .\src\sw10.min.js  published\ /y
+copy .\src\chooserclassification.js published\ /y
+copy .\src\symbolsize.js  published\ /y
+
 xcopy .\src\signmaker\*.*  published\signmaker /s /y
 rem Please commit any changes to the publish folder
 pause
