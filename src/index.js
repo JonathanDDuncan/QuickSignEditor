42b8b51ec02e84ea5155a527e3ba5018ec06301c
Jonathan Duncan <duncanjonathan@yahoo.ca>
2016-12-09 16:25:30 -0600

    Fixing bug in touch event overriding to mouse events

--- src/index.js
+++ src/index.js
@@ -242,30 +242,37 @@ app.ports.requestSignfromOtherAppDelayed.subscribe(requestSignDelayed);
 
 
 function touchHandler(event) {
-    var touches = event.changedTouches,
-        first = touches[0],
-        type = "";
-    switch (event.type) {
-        case "touchstart":
-            type = "mousedown";
-            break;
-        case "touchmove":
-            type = "mousemove";
-            break;
-        case "touchend":
-            type = "mouseup";
-            break;
-        default:
-            return;
-    }
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent(type, true, true, window, 1,
-        first.screenX, first.screenY,
-        first.clientX, first.clientY, false,
-        false, false, false, 0 /*left*/ , null);
+    var touches = event.changedTouches
+    if (touches) {
+        var first = touches[0],
+            type = "";
+        switch (event.type) {
+            case "touchstart":
+                type = "mousedown";
+                break;
+            case "touchmove":
+                type = "mousemove";
+                break;
+            case "touchend":
+                type = "mouseup";
+                break;
+            default:
+                return;
+        }
+        if (first) {
+            var simulatedEvent = document.createEvent("MouseEvent");
+            simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                first.screenX, first.screenY,
+                first.clientX, first.clientY, false,
+                false, false, false, 0 /*left*/ , null);
 
-    first.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
+            first.target.dispatchEvent(simulatedEvent);
+ 
+            event.preventDefault();
+        }
+    } else { return; }
+
+
 }
 
 function init() {
