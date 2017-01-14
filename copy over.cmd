3c717e20a7650cd39fa707cbab664301da6b3071
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-01-13 22:20:32 -0600

    Updating assets folder

--- copy over.cmd
+++ copy over.cmd
@@ -1,6 +1,7 @@
 move .\dist\js\*.js  .\dist\js\elm.js
 copy  .\dist\js\elm.js src\assets\lib\elm.js
 
+xcopy /d /s /y .\src\assets  .\published\assets
 xcopy /d /s /y .\src\assets  .\published\signmaker\assets
 xcopy /d /s /y .\src\assets  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\assets\" 
 
