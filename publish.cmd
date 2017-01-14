91e5e0f80c86567b43b450b7eec1e96b4a8fa145
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-01-13 22:08:12 -0600

    Updating assets folder

--- publish.cmd
+++ publish.cmd
@@ -1,27 +1,4 @@
-del .\published\*.css
-del .\published\*.js
-xcopy .\dist\*.*  published /s /y
-
-del src\signmaker\lib\elm.js
-
-move .\dist\js\*.js  src\signmaker\lib\elm.js
-copy .\src\styles\*.css  src\signmaker\css\
-copy .\src\styles\*.css  published\styles\
-
-copy src\signmaker\lib\elm.js "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\lib\"
-copy .\src\styles\*.css  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\css\"
-
-copy .\src\sw10.js  published\ /y
-copy .\src\sw10.min.js  published\ /y
-copy .\src\chooserclassification.js published\ /y
-copy .\src\symbolsize.js  published\ /y
-
-copy .\src\sw10.js  published\signmaker\ /y
-copy .\src\sw10.min.js  published\signmaker\ /y
-copy .\src\chooserclassification.js published\signmaker\ /y
-copy .\src\symbolsize.js  published\signmaker\ /y
-
-xcopy .\src\signmaker\*.*  published\signmaker /s /y
+"copy over.cmd"  
 rem Please commit any changes to the publish folder
 pause
 git branch -dr origin/gh-pages
