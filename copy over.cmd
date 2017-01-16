6e992800097e44c5081d45c34ddf27ddcee8b293
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-01-16 16:01:15 -0600

    Updating copy over cmd

--- copy over.cmd
+++ copy over.cmd
@@ -1,8 +1,10 @@
 move .\dist\js\*.js  .\dist\js\elm.js
 copy  .\dist\js\elm.js src\assets\lib\elm.js
 
+xcopy /d /s /y .\src\assets  .\src\signmaker\assets
 xcopy /d /s /y .\src\assets  .\published\assets
-xcopy /d /s /y .\src\assets  .\published\signmaker\assets
+xcopy /d /s /y .\src\signmaker .\published\signmaker\
+
 xcopy /d /s /y .\src\assets  "..\..\..\SignWriter\SignWriter Studio\SignWriterStudio\dist\assets\" 
 
 pause
