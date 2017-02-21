d81452be6b36b802c5beb6a47572232a187cb8c7
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-02-21 10:09:35 -0600

    update build command

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 cd scripts
 npm run build
 
-del C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\lib\elm.js
-copy .\dist\*.js  C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\lib\elm.js
-copy .\src\styles\*.css  C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\css\
+rem del C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\lib\elm.js
+rem copy .\dist\*.js  C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\lib\elm.js
+rem copy .\src\styles\*.css  C:\Users\Jonathan\Documents\Freelance\SignWriterStudio\signmaker\css\
 pause
