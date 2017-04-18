017cca82bc3eb86ea914afe6f1c295f949664173
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-04-17 20:18:33 -0600

    Add symbol delete to backspace key

--- src/SWEditor/EditorKeyboard.elm
+++ src/SWEditor/EditorKeyboard.elm
@@ -67,7 +67,23 @@ otherkeys =
                     ]
             }
       }
-    , { test = { key = 62, ctrl = False, shift = False, alt = False }
+    , { test =
+            -- Delete key
+            { key = 62, ctrl = False, shift = False, alt = False }
+      , action = (DeleteSymbols)
+      , display =
+            { width = 24
+            , height = 24
+            , view =
+                svg [ attribute "height" "24", attribute "viewBox" "0 0 24 24", attribute "width" "24", attribute "xmlns" "http://www.w3.org/2000/svg" ]
+                    [ path [ attribute "d" "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" ]
+                        []
+                    ]
+            }
+      }
+    , { test =
+            -- Backspace
+            { key = 14, ctrl = False, shift = False, alt = False }
       , action = (DeleteSymbols)
       , display =
             { width = 24
