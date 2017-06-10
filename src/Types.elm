9853c900a27e2555d923cb7e650bc89420365055
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-06-09 19:57:48 -0600

    refactor

--- src/Types.elm
+++ src/Types.elm
@@ -1,22 +1,12 @@
 module Types exposing (Model, Msg(..))
 
--- import Feature.Types
-
 import Overlay.State
 
 
 type alias Model =
-    { overlay :
-        Overlay.State.Model
-        --Nest feature object to model by adding
-        -- featureFieldName : Feature.Types.Model
+    { overlay : Overlay.State.Model
     }
 
 
 type Msg
     = Overlay Overlay.State.Msg
-
-
-
--- Nest feature messages by adding
--- | FeatureMsg Feature.Types.Msg
