a6a6146c4e9f20c0461ec16fbcf96e37cf35e70d
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-06-19 16:53:25 -0600

    Add FSW export styling for symbols

--- src/SW/FSW.elm
+++ src/SW/FSW.elm
@@ -1,6 +1,7 @@
 module SW.FSW exposing (getFsw)
 
 import SW.Sign exposing (Sign, Lane, getlane, lanes, signinit, centerSign)
+import SW.Symbol exposing (Symbol)
 
 
 getFsw : Sign -> String
@@ -18,10 +19,95 @@ getFsw sign =
 
         symbols =
             List.foldr (++) "" (List.map symbolsFsw centered.syms)
+
+        styling =
+            symbolstylingFsw centered.syms
+
+        fsw =
+            Debug.log "getFsw" <| boundingbox ++ symbols ++ styling
     in
-        boundingbox ++ symbols
+        fsw
 
 
-symbolsFsw : { c | key : String, x : a, y : b } -> String
+symbolsFsw : Symbol -> String
 symbolsFsw symbol =
     symbol.key ++ toString symbol.x ++ "x" ++ toString symbol.y
+
+
+symbolstylingFsw : List Symbol -> String
+symbolstylingFsw symbols =
+    let
+        zoom =
+            zoomstyling symbols
+
+        colors =
+            colorstyling symbols
+    in
+        if zoom ++ colors == "" then
+            ""
+        else
+            "--" ++ zoom ++ colors
+
+
+colorstyling : List Symbol -> String
+colorstyling symbols =
+    List.foldr (++) "" (List.indexedMap colorstyle symbols)
+
+
+colorstyle : Int -> Symbol -> String
+colorstyle index symbol =
+    let
+        black =
+            if symbol.nbcolor /= "black" then
+                symbol.nbcolor
+            else
+                ""
+
+        white =
+            if symbol.nwcolor /= "white" then
+                symbol.nwcolor
+            else
+                ""
+
+        symboltocolor =
+            "C" ++ twodigits (index + 1)
+    in
+        if black ++ white == "" then
+            ""
+        else if white == "" then
+            symboltocolor ++ "_" ++ black ++ "_"
+        else if black == "" then
+            symboltocolor ++ "_" ++ "black," ++ white ++ "_"
+        else
+            symboltocolor ++ "_" ++ black ++ "," ++ white ++ "_"
+
+
+zoomstyling : List { a | size : number } -> String
+zoomstyling symbols =
+    List.foldr (++) "" (List.indexedMap zoomstyle symbols)
+
+
+zoomstyle : Int -> { a | size : number } -> String
+zoomstyle index symbol =
+    if symbol.size /= 1 then
+        "Z" ++ twodigits (index + 1) ++ "," ++ toString symbol.size
+    else
+        ""
+
+
+twodigits : Int -> String
+twodigits value =
+    let
+        str =
+            toString value
+
+        len =
+            String.length str
+
+        toadd =
+            2 - len
+    in
+        List.range 1 toadd
+            |> List.map (\_ -> "0")
+            |> List.foldr (++) ""
+            |> (\chain -> chain ++ str)
